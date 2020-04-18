5d14642fc714bbfdf15c4e6811ee51ecb9686b39
Adrian Thomas <thomasa8992@hotmail.com>
2020-04-18 02:42:59 -0500

    Refactored logic that makes the player jump and logic that detects player collision wuth the ground, also added a physics property

--- Orb Attack/Assets/Player.cs
+++ Orb Attack/Assets/Player.cs
@@ -6,25 +6,36 @@ public class Player : MonoBehaviour
 {
     private Rigidbody playerRigidBody;
     private bool isPlayerOnTheGround = false;
+    private float jumpForce = 10;
+    public float gravityModifier = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space) && isPlayerOnTheGround) {
+    void Update() {
+        ControlPlayerJump();
+    }
+
+    private void ControlPlayerJump() {
+        if (Input.GetKeyDown(KeyCode.Space) && isPlayerOnTheGround) {
             isPlayerOnTheGround = false;
 
-            playerRigidBody.AddForce((Vector3.up * 10) / 2, ForceMode.Impulse);
+            playerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 
     private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.name == "Plane") {
+        HandlePlayerCollisionWithGround(other);
+    }
+
+    private void HandlePlayerCollisionWithGround(Collision other) {
+        if (other.gameObject.name == "Plane") {
             isPlayerOnTheGround = true;
             Debug.Log(isPlayerOnTheGround);
         }
