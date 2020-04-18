4e121d13dc99694f20e56b71264d3814eccb87b6
Adrian Thomas <thomasa8992@hotmail.com>
2020-04-18 02:29:09 -0500

    Added player script and player object as well as a plane, in player script, added jump logic on space bar key down

---
+++ Orb Attack/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody playerRigidBody;
+    private bool isPlayerOnTheGround = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isPlayerOnTheGround) {
+            isPlayerOnTheGround = false;
+
+            playerRigidBody.AddForce((Vector3.up * 10) / 2, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.name == "Plane") {
+            isPlayerOnTheGround = true;
+            Debug.Log(isPlayerOnTheGround);
+        }
+    }
+}
